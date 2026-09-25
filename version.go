8f679b0c07b8a156435f3591b24c507efc631a21
David <deivid.garcia.garcia@gmail.com>
2026-09-25 19:19:23 +0200

    🔖 release v1.9.2

--- version.go
+++ version.go
@@ -2,4 +2,4 @@ package crowdsec_bouncer_traefik_plugin //nolint:revive,stylecheck
 
 // pluginVersion is what the plugin reports to the Crowdsec LAPI.
 // Do not edit by hand: the "Release (1/2) Prepare" workflow bumps it.
-var pluginVersion = "v1.9.1" //nolint:gochecknoglobals
+var pluginVersion = "v1.9.2" //nolint:gochecknoglobals
